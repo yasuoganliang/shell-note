a05da9a3aa113c45b65c3566081d8cd20139b350
yasuoganliang <610671733@qq.com>
2019-10-11 23:48:51 +0800

    f(string): Strip all instances of pattern from string

--- string/06-去除字符串中所有的匹配项/strip_all.sh
+++ string/06-去除字符串中所有的匹配项/strip_all.sh
@@ -6,6 +6,7 @@ strip_all() {
 	# Usage: strip_all "string" "pattern"
 	# ${parameter//pattern/string} 会把所有的匹配项都替换掉
 	# 此处把第一个参数中所有匹配的第二个参数的项全部替换为空
+	# 一个斜杆会替换第一个匹配的字符
 	printf '%s\n' "${1//$2}"
 }
 
